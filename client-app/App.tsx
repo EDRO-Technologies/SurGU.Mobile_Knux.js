7f02f4d1fe479f4a33ff40347a585219524bd664
GalaxyShad <vadim.kadyrin@mail.ru>
2023-11-12 03:58:38 +0500

    page change

--- client-app/App.tsx
+++ client-app/App.tsx
@@ -2,11 +2,15 @@
 // import { Text, View } from 'react-native';
 // import View1 from './src/views/View1';
 // import Chat from './src/views/Chat'
+import { Appbar, Button } from "react-native-paper";
+import { Avatar, Card, IconButton, TextInput, } from "react-native-paper";
+import { View, TouchableOpacity } from "react-native";
 
 import { RegistratePage } from "./src/views/RegistratePage";
-import { HomePage } from "./src/views/HomePage";
+// import { HomePage } from "./src/views/HomePage";
 import { ChatPage } from "./src/views/ChatPage";
 import { ViewPage } from "./src/views/ViewPage";
+import { PaperProvider } from "react-native-paper";
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
@@ -15,20 +19,37 @@ const Stack = createNativeStackNavigator();
 
 
 export default function App() {
-    
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomePage}></Stack.Screen>
-        <Stack.Screen name="Chat" component={ViewPage}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomePage}/>
+                <Stack.Screen name="Registrate" component={RegistratePage}/>
+                <Stack.Screen name="Chat" component={ViewPage}/>
+            </Stack.Navigator>
+        </NavigationContainer>
   );
 }
 
+const HomePage = ({ navigation }) => {
+    return (
+        <View className="flex-1 bg-slate-100 w-screen">
+            {CardTitleChat("gg", "wpjj", navigation.navigate)}
+        </View> 
+    );
+};
 
-
-
+const CardTitleChat = (nameChat: string, lastMessage: string, navig: Function) => {
+    return(<TouchableOpacity onPress={() => navig('Chat')}>
+      <Card.Title
+        className="border border-slate-100 bg-white"
+        title={"Chat: " + nameChat}
+        subtitle={"Message: " + lastMessage}
+        left={(props) => <Avatar.Icon {...props} icon="folder" />}
+        // right={(props) => <IconButton {...props} icon="dots-vertical" onPress={() => {}} />}
+      />
+    </TouchableOpacity>
+    );
+};
 
 
 // export default function App() {
--- client-app/src/views/HomePage.tsx
+++ client-app/src/views/HomePage.tsx
@@ -1,50 +1,53 @@
-import React, { useState, useCallback, useEffect } from "react";
-import { GiftedChat } from "react-native-gifted-chat";
-import { Text, View } from "react-native";
-import { PaperProvider } from "react-native-paper";
-import { ActivityIndicator, MD2Colors } from "react-native-paper";
-import { Appbar, Button } from "react-native-paper";
-import { Avatar, Card, IconButton, TextInput } from "react-native-paper";
+// import React, { useState, useCallback, useEffect } from "react";
+// import { GiftedChat } from "react-native-gifted-chat";
+// import { Text, View } from "react-native";
+// import { ActivityIndicator, MD2Colors } from "react-native-paper";
+// import { Appbar, Button } from "react-native-paper";
+// import { Avatar, Card, IconButton, TextInput } from "react-native-paper";
 
-export const HomePage = () => {
-  return (
-    <PaperProvider>
-      {HeaderChat()}
-      <View className="flex-1 bg-slate-100 w-screen">
-        {ButtonComponent()}
-        {CardTitleChat("gg", "wpjj")}
-        {CardTitleChat("njnk", "gcfx")}
-        {/* {chatList} */}
-      </View>
-    </PaperProvider>
-  );
-};
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack'
+// const Stack = createNativeStackNavigator();
 
-function CreateChat() {
-  return CardTitleChat("hh", "kok");
-}
+// export const HomePage = ({ navigation }) => {
+//   return (
+//       // {HeaderChat()}
+//       <View className="flex-1 bg-slate-100 w-screen">
+//         {/* {ButtonComponent()} */}
+//         {CardTitleChat("gg", "wpjj", navigation )}
+//         {/* {CardTitleChat("njnk", "gcfx")} */}
+//         {/* {chatList} */}
+//       </View>
+//   );
+// };
 
-const ButtonComponent = () => (
-  <Button icon="camera" mode="contained" onPress={() => CreateChat("hh", "kk")}>
-    Add Chat
-  </Button>
-);
+// // function CreateChat() {
+// //   return CardTitleChat("hh", "kok");
+// // }
 
-const CardTitleChat = (nameChat: string, lastMessage: string) => (
-  <Card.Title
-    className="border border-slate-100 bg-white"
-    title={"Chat: " + nameChat}
-    subtitle={"Message: " + lastMessage}
-    left={(props) => <Avatar.Icon {...props} icon="folder" />}
-    // right={(props) => <IconButton {...props} icon="dots-vertical" onPress={() => {}} />}
-  />
-);
+// // const ButtonComponent = () => (
+// //   <Button icon="camera" mode="contained" onPress={() => CreateChat("hh", "kk")}>
+// //     Add Chat
+// //   </Button>
+// // );
 
-const HeaderChat = (nameTitle: string = "Welcom to Clab") => (
-  <Appbar.Header>
-    {/* <Appbar.BackAction onPress={() => {}} /> */}
-    <Appbar.Content title={nameTitle} />
-    {/* <Appbar.Action icon="calendar" onPress={() => {}} /> */}
-    {/* <Appbar.Action icon="magnify" onPress={() => {}} /> */}
-  </Appbar.Header>
-);
+// const CardTitleChat = (nameChat: string, lastMessage: string, navigat: function) => (
+//   <Button mode="contained" onPress={() => navigat}>
+//     <Card.Title
+//       className="border border-slate-100 bg-white"
+//       title={"Chat: " + nameChat}
+//       subtitle={"Message: " + lastMessage}
+//       left={(props) => <Avatar.Icon {...props} icon="folder" />}
+//       // right={(props) => <IconButton {...props} icon="dots-vertical" onPress={() => {}} />}
+//     />
+//   </Button>
+// );
+
+// const HeaderChat = (nameTitle: string = "Welcom to Clab") => (
+//   <Appbar.Header>
+//     {/* <Appbar.BackAction onPress={() => {}} /> */}
+//     <Appbar.Content title={nameTitle} />
+//     {/* <Appbar.Action icon="calendar" onPress={() => {}} /> */}
+//     {/* <Appbar.Action icon="magnify" onPress={() => {}} /> */}
+//   </Appbar.Header>
+// );
