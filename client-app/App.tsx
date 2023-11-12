68ce913e9aac30e57d71dd444a344fb5c6598188
GalaxyShad <vadim.kadyrin@mail.ru>
2023-11-12 06:49:58 +0500

    Top bar

--- client-app/App.tsx
+++ client-app/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { Appbar, BottomNavigation, Button, Avatar, Card, IconButton, TextInput, PaperProvider } from "react-native-paper";
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -24,11 +24,19 @@ function SettingsScreen() {
   );
 }
 
+function ContactScreen(){
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>List User!</Text>
+    </View>
+  );
+}
+
 function HomeScreen(){
   return(
             <Stack.Navigator>
               {/* <Stack.Screen name="Login" component={LoginPage}/> */}
-              <Stack.Screen name="Home" component={HomePage}/>
+              <Stack.Screen name="Home" component={HomePage} options={{headerShown: false}}/>
               <Stack.Screen name="Chat" component={ViewPage}/>
             </Stack.Navigator>
   );
@@ -38,13 +46,26 @@ export default function App() {
   return (
           <NavigationContainer>
             <Tab.Navigator>
-              <Tab.Screen name="HomeWorld" component={HomeScreen} />
-              <Tab.Screen name="Settings" component={SettingsScreen} />
+              <Tab.Screen name="Home"     component={HomeScreen}     options={{ headerTitle: (props) => <LogoTitle {...props} /> }}/>
+              <Tab.Screen name="Contacts" component={ContactScreen}  options={{ headerTitle: (props) => <LogoTitle {...props} /> }}/>
+              <Tab.Screen name="Settings" component={SettingsScreen} options={{ headerTitle: (props) => <LogoTitle {...props} /> }}/>
             </Tab.Navigator>
           </NavigationContainer>
   );
 }
 
-
+function LogoTitle() {
+  return (
+    <View className="flex-row w-screen justify-around items-center">
+      <Text></Text>
+      <Text>AAAAAAAdfffffffffffffffAAa</Text>
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={require('../client-app/assets/icon.png')}
+      />
+    </View>
+    
+  );
+}
 
 
